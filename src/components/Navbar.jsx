a2389c6d549d0ccb29a33534b90612da805a76a9
InvilleXXV <allangomes6090@gmail.com>
2026-06-21 15:13:19 -0300

    primeiro commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        <header>
+            <h1>Vanderley Driver</h1>
+
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre mim</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
