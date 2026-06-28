b013949bebdc63a9bcdb823f1ea827ebc540abe2
InvilleXXV <allangomes6090@gmail.com>
2026-06-28 20:55:19 -0300

    Finaliza estrutura da Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
 function Navbar() {
     return (
         <header>
+
+            <div>
             <h1>Vanderley Driver</h1>
+            </div>
 
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Sobre mim</li>
-                    <li>Contato</li>
+                    <li>
+                    <a href="#home">Home</a>
+                    </li>
+
+                    <li>
+                    <a href="#sobre">Sobre mim</a>
+                    </li>
+                    
+                    <li>
+                    <a href="#contato">Contato</a>
+                    </li>
                 </ul>
             </nav>
+
+            <button>
+               ☰
+            </button>
+
         </header>
     )
 }
