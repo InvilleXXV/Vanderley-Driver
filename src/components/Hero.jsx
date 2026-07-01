618b24e768918943ee7af48726f9f2bb1130466e
InvilleXXV <allangomes6090@gmail.com>
2026-06-30 21:00:02 -0300

    Inicia estrutura do Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+function Hero() {
+    return (
+      <section>
+
+
+        <div>
+        <h1>Conforto, Segurança e Pontualidade em cada viagem.</h1>
+
+        <p>
+        Pontualidade para viagem urbanas, aeroportos e atendimento executivo.
+        </p>
+
+        <button>Local</button>
+        <button>WhatsApp</button>
+        </div>
+<div>
+    
+</div>
+
+      </section>
+     );
+}
+export default Hero;
